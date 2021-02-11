cc7a29c66be8429f67d67e658f69fb2cf07d4e43
Portafolio-web <78131157+Portafolio-web@users.noreply.github.com>
2021-02-10 20:14:12 -0500

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -3,7 +3,8 @@
  $nombre = ($_POST["nombre"]);
  $correo = ($_POST["correo"]);
  $mensaje = ($_POST["mensaje"]);
- $contenido = "\nnombre: " . $nombre . "\ncorreo: " . $correo . "\ncontacto: " . $contacto . "\nmensaje: " . $mensaje;
+ $contenido = "\nnombre: " . $nombre . "\ncorreo: " . $correo . "\nmensaje: " . $mensaje;
  mail($destino, "contacto desde nuestra web", $contenido); 
   header("location:index.html");
   ?>
+  
